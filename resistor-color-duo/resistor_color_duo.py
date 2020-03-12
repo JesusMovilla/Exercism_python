ef9f2cb612fd4a3e9d7986aa48204365e299b731
Jesus Movilla <jesumogo2000@gmail.com>
2020-03-11 19:53:45 -0500

    Solución

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,7 @@
+colors_code = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue',
+               'violet', 'grey', 'white']
+
+
+def value(colors):
+    return int(str(colors_code.index((colors[0:1])[0])) +
+               str(colors_code.index((colors[1:2])[0])))
