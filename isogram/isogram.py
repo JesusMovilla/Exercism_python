dae5b39bea0740624bc9f4b47274e9d2547affa0
JesusMovilla <60763635+JesusMovilla@users.noreply.github.com>
2020-03-13 17:58:30 -0500

    Delete isogram.py

--- isogram/isogram.py
+++
@@ -1,8 +0,0 @@
-def is_isogram(string):   
-    exception=" ,_-"
-    cont=[i.lower() for i in string if i not in exception]
-    for i in cont:
-     if cont.count(i)>1:
-         return False
-    else:
-         return True
