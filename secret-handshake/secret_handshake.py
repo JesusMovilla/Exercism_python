948c15727fb72cd28c4246f7b143907e9ff13085
JesusMovilla <60763635+JesusMovilla@users.noreply.github.com>
2020-03-13 17:21:53 -0500

    Delete secret_handshake.py

--- secret-handshake/secret_handshake.py
+++
@@ -1,5 +0,0 @@
-def commands(number):
-    clave = ['wink', 'double blink', 'close your eyes', 'jump']
-    combinacion = [action for index,
-                   action in enumerate(clave) if 2 ** index & number]
-    return combinacion[::-1] if 2 ** 4 & number else combinacion
