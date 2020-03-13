d6097efac6c9033a3e8cd445a7f33efb04f1460f
JesusMovilla <60763635+JesusMovilla@users.noreply.github.com>
2020-03-13 17:53:55 -0500

    Delete flatten_array.py

--- flatten-array/flatten_array.py
+++
@@ -1,8 +0,0 @@
-def flatten(iterable):
-    lista = list()
-    for item in iterable:
-        if isinstance(item, (list, tuple)):
-            lista += flatten(item)
-        elif item is not None:
-            lista.append(item)
-    return lista
